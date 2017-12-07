78795d460ca34762c71a2157068db710e9fbdd03
Pandeul <pandeul@MacBook-Pro-de-Pandeul.local>
2017-12-08 00:49:40 +0100

    finalisation etape 1 et 2

--- eeTest/ee_8.js
+++ eeTest/ee_8.js
@@ -7,6 +7,17 @@ $(document).ready(function() {
 		if(times != Sha1.hash(nbTimes)){
 			audio.play();
 			nbTimes++;
+
+			if(Sha1.hash(nbTimes) == 'ac3478d69a3c81fa62e60f5c3696165a4e5e6ac4')
+				$(document.body).append("<p>Y'a une fin...</p>");
+			else if(Sha1.hash(nbTimes) == 'f1abd670358e036c31296e66b3b66c382ac00812')
+				$(document.body).append("<p>Promis, y'a une fin...</p>");
+			else if(Sha1.hash(nbTimes) == 'f6e1126cedebf23e1463aee73f9df08783640400')
+				$(document.body).append("<p>Vous y etes bientot</p>");
+			else if(Sha1.hash(nbTimes) == '972a67c48192728a34979d9a35164c1295401b71')
+				$(document.body).append("<p>Allez, encore un effort</p>");
+			else if(Sha1.hash(nbTimes) == 'af3e133428b9e25c55bc59fe534248e6a0c0f17b')
+				$(document.body).append("<p>Plus que 2</p>");
 		}
 		else{
 			clearInterval(test);
