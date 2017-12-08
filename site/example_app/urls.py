e9c86aed1a48fc7cb877900a74706dde3d163c1c
Pandeul <pandeul@MacBook-Pro-de-Pandeul.local>
2017-12-08 04:43:02 +0100

    ajout des etapes jusqu'a 5 de l'easter egg

--- site/example_app/urls.py
+++ site/example_app/urls.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from chatterbot.ext.django_chatterbot import urls as chatterbot_urls
 from example_app.views import ChatterBotAppView
 from example_app.views import ee_1_2
+from example_app.views import ee_3
+from example_app.views import ee_4
 
 urlpatterns = [
     url(r'^$', ChatterBotAppView.as_view(), name='main'),
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^api/chatterbot/', include(chatterbot_urls, namespace='chatterbot')),
     url(r'^404over/', ee_1_2.as_view(), name='main2'),
+    url(r'^qshzljhqfu/', ee_3.as_view(), name='main3'),
+    url(r'^DRA7W/', ee_4.as_view(), name='main4'),
 
 ]
--- site/example_app/views.py
+++ site/example_app/views.py
@@ -6,3 +6,9 @@ class ChatterBotAppView(TemplateView):
 
 class ee_1_2(TemplateView):
 	template_name = "ee_2_3.html"
+
+class ee_3(TemplateView):
+	template_name = "ee_4.html"
+
+class ee_4(TemplateView):
+	template_name = "ee_5.html"	
